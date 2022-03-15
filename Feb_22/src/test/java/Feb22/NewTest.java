bb2994d12c7bf2ab85644c9738a4e9caf4b52710
ChetanaCVenkat <99272316+ChetanaCVenkat@users.noreply.github.com>
2022-03-15 18:11:22 +0530

    Update NewTest.java
    testaddddddddddddddiiiiiiiiiiiiiiiii

--- Feb_22/src/test/java/Feb22/NewTest.java
+++ Feb_22/src/test/java/Feb22/NewTest.java
@@ -1,48 +1,51 @@
 package Feb22;
 
-
 import org.apache.logging.log4j.LogManager;
 
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class NewTest {
 
-		
-	private static Logger log=LogManager.getLogger(NewTest.class);
-	  @Test
-	  public void setup() {
-		  
-		  System.setProperty("webdriver.chrome.driver","D:\\Driver\\chromedriver_win32\\chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
-			//comment the above 2 lines and uncomment below 2 lines to use Chrome
-			//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-			//WebDriver driver = new ChromeDriver();
-	  	
-			log.info("CHETANA");
-			
-	      String baseUrl = "http://demo.guru99.com/test/newtours/";
-	      String expectedTitle = "Welcome: Mercury Touars";
-	      String actualTitle = "";
+	WebDriver driver;
+	private static Logger log = LogManager.getLogger(NewTest.class);
 
-	      // launch Fire fox and direct it to the Base URL
-	      driver.get(baseUrl);
+	@BeforeClass
+	public void setup() {
 
-	      // get the actual value of the title
-	      actualTitle = driver.getTitle();
+		System.setProperty("webdriver.chrome.driver", "D:\\Driver\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		// comment the above 2 lines and uncomment below 2 lines to use Chrome
+		// System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+		// WebDriver driver = new ChromeDriver();
 
-	      /*
-	       * compare the actual title of the page with the expected one and print
-	       * the result as "Passed" or "Failed"
-	       */
-	      if (actualTitle.contentEquals(expectedTitle)){
-	          System.out.println("Test Passed!");
-	      } else {
-	          System.out.println("Test Failed");
-	      }
-	  }
+		log.info("CHETANA");
 	}
 
+	@Test
+	public void loginPage() {
 
+		String baseUrl = "http://demo.guru99.com/test/newtours/";
+		String expectedTitle = "Welcome: Mercury Touars";
+		String actualTitle = "";
+		log.info("Hi chetana");
+		// launch Fire fox and direct it to the Base URL
+		driver.get(baseUrl);
+
+		// get the actual value of the title
+		actualTitle = driver.getTitle();
+
+		/*
+		 * compare the actual title of the page with the expected one and print the
+		 * result as "Passed" or "Failed"
+		 */
+		if (actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("Test Passed!");
+		} else {
+			System.out.println("Test Failed");
+		}
+	}
+}
